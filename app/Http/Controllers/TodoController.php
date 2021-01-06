e7a8d2491a8bc723fc328f4169cddc0d9bbe76fc
Kruthal <kruthal@kruthal.com>
2021-01-06 16:11:56 +0200

    Todo List Mini Project 4 | Validations

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -18,9 +18,22 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        if(!$request->title){
-            return redirect()->back()->with('error', 'Please give title');
+        $rules = [
+            'title' => 'required|max:255',
+        ];
+        $messages = [
+            'title.max' => 'Todo title should not be greater than 255 chars.',
+        ]
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
         }
+        //$request->validate([
+        //    'title' => 'required|max:255',
+        //]);
+        //if(!$request->title){
+        //    return redirect()->back()->with('error', 'Please give title');
+        //}
         Todo::create($request->all());
         return redirect()->back()->with('message','Todo Created Successfully');
     }
