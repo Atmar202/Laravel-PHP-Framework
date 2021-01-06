54eaa81ddfe8beaec322ebe7dbaa1b57e40eeecb
Kruthal <kruthal@kruthal.com>
2021-01-06 17:06:06 +0200

    Todo List Mini Project 6 | Show All Todo

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <title>Todos</title>
-</head>
-<body>
-    <div class="text-center pt-10">
+@extends('todos.layout')
+
+@section('content')
         <h1 class="text-2xl">What next you need To-DO</h1>
         <x alert />
         <form method="post" action="/todo/create" class="py-5">
@@ -15,6 +8,4 @@
         <input type="text" name="title" class="py-2 px-2 border rounded" />
         <input type="submit" value="Create" class="p-2 border rounded" />
     </form>
-</div>
-</body>
-</html>
+@endsection
---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,12 @@
+@extends('todos.layout')
+
+@section('content')
+<h1 class="text-2xl">All your todos</h1>
+        <ul>
+            @foreach($todos as $todo)
+            <li>
+                {{$todo->title }}
+            </li>
+            @endforeach
+            <ul>
+@endsection
