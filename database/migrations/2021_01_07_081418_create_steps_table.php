7680aeab20e3cb1b4feeb3598719bda15f889cac
Kruthal <kruthal@kruthal.com>
2021-01-07 10:22:13 +0200

    Todo List Mini Project 27 | Todo has Many Steps

--- database/migrations/2021_01_07_081418_create_steps_table.php
+++ database/migrations/2021_01_07_081418_create_steps_table.php
@@ -3,9 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Http\Request;
 
-class CreateUsersTable extends Migration
+class CreateStepsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('steps', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar')->nullable();
-            $table->rememberToken();
+            $table->unsignedBigInteger('todo_id');
+            $table->foreign('todo_id')->references('id')->on('todos');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
@@ -33,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('steps');
     }
 }
