4ba05bb3c9731df9d737f51785bf66dbfbd622c2
Kruthal <kruthal@kruthal.com>
2021-01-07 08:13:18 +0200

    Todo List Mini Project 23 | Introduction to Laravel LiveWire

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -13,7 +13,15 @@
             <input type="text" name="title" class="py-2 px-2 border rounded" placeholder="title" />
         </div>
         <div class="py-1">
-            <textarea name="description" class="p-2 rounded border" placeholder="description"></textarea>
+            <textarea name="description" name="description" class="py-2 px-2 border rounded" placeholder="description"></textarea>
+        </div>
+
+        <div class="py-2">
+        <div class="flex justify-center pb-4 px-4">
+        <h2 class="text-lg pb-4">Add Steps for Task</h1>
+        <span class="fas fa-plus px-2 py-1 cursor-pointer"></span>
+        </div>
+        <input type="text" name="step" class="py-2 px-2 border rounded" placeholder="Describe Step" />
         </div>
         <div class="py-1">
             <input type="submit" value="Create" class="p-2 border rounded" />
